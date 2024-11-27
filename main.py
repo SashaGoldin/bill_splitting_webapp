a65ee3a68b40f885404025d8730a4a15c20cd1a4
SashaGoldin <sasha.a.goldin@gmail.com>
2024-11-27 04:45:41 -0600

    final push

--- main.py
+++ main.py
@@ -17,28 +17,39 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template("bill_form_page.html", billform=bill_form)
 
+    def post(self):
+        billform = BillForm(request.form)
+        the_bill = Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+        return render_template("bill_form_page.html", result=True, billform=billform, name1=flatmate1.name, amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name, amount2=flatmate2.pays(the_bill, flatmate1))
+
 
 class ResultsPage(MethodView):
     def post(self):
         billform = BillForm(request.form)
-        amount = billform.amount.data
-        return f"hello you awe {amount} dollars"
+        the_bill = Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+        return render_template("results.html", name1=flatmate1.name, amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name, amount2=flatmate2.pays(the_bill, flatmate1))
 
 class BillForm(Form):
-    amount = StringField("Bill amount: ")
-    period = StringField("Bill period: : ")
+    amount = StringField("Bill amount: ", default="100")
+    period = StringField("Bill period: : ", default="December 2024")
 
-    name1 = StringField("Name: ")
-    days_in_house1 = StringField("Days in house: ")
+    name1 = StringField("Name: ", default="John")
+    days_in_house1 = StringField("Days in house: ", default="20")
 
-    name2 = StringField("Name: ")
-    days_in_house2 = StringField("Days in house: ")
+    name2 = StringField("Name: ", default="Marry")
+    days_in_house2 = StringField("Days in house: ", default='12')
 
     button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+# app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
